343633d53aefa36acabf7bd74da48f5e274337ec
Namju Kim <cmsong111@naver.com>
2024-04-06 18:30:15 +0900

    Project: Upgrade Springboot Version and Add Spring Security

---
+++ src/main/java/deu/cse/spring_webmail/configuration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package deu.cse.spring_webmail.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        // TODO: Add authentication and authorization rules
+        http
+                .csrf(AbstractHttpConfigurer::disable
+                )
+                .headers((headers) ->
+                        headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable
+                        )
+                )
+                .authorizeHttpRequests((authorizeRequests) ->
+                        authorizeRequests
+                                .anyRequest().permitAll()
+                );
+        return http.build();
+    }
+
+    // Spring Security "//" double slash issue
+    @Bean
+    public HttpFirewall allowUrlEncodedDoubleSlashHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowUrlEncodedDoubleSlash(true);
+        return firewall;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.httpFirewall(
+                allowUrlEncodedDoubleSlashHttpFirewall());
+    }
+}
