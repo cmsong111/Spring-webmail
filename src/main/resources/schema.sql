3c6b69aba52a14e0484e097393feff0d77174996
Namju Kim <cmsong111@naver.com>
2024-05-07 14:52:54 +0900

    Fix: Data.sql 파일 수정
    첫 실행 & 재 실행 시에 데이터가 어드민 계정이 자동으로 추가되도록 설정하였음

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-create table User_roles
+create table IF NOT EXISTS User_roles
 (
     roles          tinyint check (roles between 0 and 1),
     User_USER_NAME varchar(255) not null,
     FOREIGN KEY (User_USER_NAME) REFERENCES JAMES_USER(USER_NAME)
-)
+);
