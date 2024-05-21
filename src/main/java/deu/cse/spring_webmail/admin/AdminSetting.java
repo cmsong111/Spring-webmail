7172cc6169b3d00c89bfc7a7ac111d84f906765c
Namju Kim <cmsong111@naver.com>
2024-05-21 17:12:59 +0900

    Fix: Admin 권한 부여 에러 수정

--- src/main/java/deu/cse/spring_webmail/admin/AdminSetting.java
+++ src/main/java/deu/cse/spring_webmail/admin/AdminSetting.java
@@ -3,10 +3,16 @@ package deu.cse.spring_webmail.admin;
 import deu.cse.spring_webmail.james.JamesAdminMailBox;
 import deu.cse.spring_webmail.james.JamesAdminUser;
 import deu.cse.spring_webmail.mail.dto.MailBoxType;
+import deu.cse.spring_webmail.user.Role;
+import deu.cse.spring_webmail.user.User;
+import deu.cse.spring_webmail.user.UserRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Slf4j
 @Component
@@ -15,6 +21,8 @@ public class AdminSetting {
 
     private final JamesAdminMailBox jamesAdminMailBox;
     private final JamesAdminUser jamesAdminUser;
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     /**
      * 프로그램 시작시 admin 계정 생성 및 관련 메일박스 생성
@@ -27,11 +35,21 @@ public class AdminSetting {
             log.info("admin user already exists");
         } else {
             log.info("admin user does not exist");
+            // For James
             jamesAdminUser.createUser("admin", "admin");
             jamesAdminMailBox.createMailBox("admin", MailBoxType.INBOX.getMailBoxName());
             jamesAdminMailBox.createMailBox("admin", MailBoxType.SENT.getMailBoxName());
             jamesAdminMailBox.createMailBox("admin", MailBoxType.TRASH.getMailBoxName());
             jamesAdminMailBox.createMailBox("admin", MailBoxType.DRAFT.getMailBoxName());
+
+            // For Spring Database
+            User user = userRepository.findByUserName("admin").get();
+            user.setPassword(passwordEncoder.encode("admin"));
+            List<Role> roles = user.getRoles();
+            roles.add(Role.ADMIN);
+            roles.add(Role.USER);
+            user.setRoles(roles);
+            userRepository.save(user);
         }
     }
 }
